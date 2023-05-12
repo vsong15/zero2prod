5cfe3f9c5d2f50b8d0238954b8a5f060d766e4c5
Vincent Song <dela.vincent.15@gmail.com>
2023-05-12 17:00:05 -0400

    Test Isolation

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,40 @@
+use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String
+}
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>, // Renamed
+) -> HttpResponse {
+    // `Result` has two variants: `Ok` and `Err`.
+    // The first for successes, the second for failures.
+    // We use a `match` statement to choose what to do based
+    // on the outcome.
+    // We will talk more about `Result` going forward!
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.as_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
