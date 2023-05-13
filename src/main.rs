e2e4712578095b126b08fecb6009b59797eac38a
Vincent Song <dela.vincent.15@gmail.com>
2023-05-13 12:30:12 -0400

    The Facade Pattern

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Renamed!
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,20 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-        .route("/health_check", web::get().to(health_check))
-        .route("/subscriptions", web::post().to(subscribe))
-        .app_data(db_pool.clone())
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
