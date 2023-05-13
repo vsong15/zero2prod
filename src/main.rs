53c4f3db9cd416d04d2b7b8fe3dc23d660baee00
Vincent Song <dela.vincent.15@gmail.com>
2023-05-13 13:36:31 -0400

    Cleaning Up Initialization

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use sqlx::PgPool;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // Renamed!
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,13 +12,20 @@ pub struct FormData {
 
 pub async fn subscribe(
     form: web::Form<FormData>,
-    pool: web::Data<PgPool>, // Renamed
+    pool: web::Data<PgPool>, 
 ) -> HttpResponse {
-    // `Result` has two variants: `Ok` and `Err`.
-    // The first for successes, the second for failures.
-    // We use a `match` statement to choose what to do based
-    // on the outcome.
-    // We will talk more about `Result` going forward!
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );        
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );        
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -28,12 +36,15 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.as_ref())
-    .await
+        .execute(pool.as_ref())
+        .instrument(query_span)
+        .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => { 
+            HttpResponse::Ok().finish() 
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
