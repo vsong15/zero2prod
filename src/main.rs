fd5e845d5f4c174467f7cc4061f957a310d2a881
Vincent Song <dela.vincent.15@gmail.com>
2023-05-14 19:21:56 -0400

    Database Connectivity

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -11,9 +11,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-        )
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
