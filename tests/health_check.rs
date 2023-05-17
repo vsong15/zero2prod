6bd733744e2bc4955c6c3b501c5bed47a1631131
Vincent Song <dela.vincent.15@gmail.com>
2023-05-17 18:40:39 -0400

    Error As Values - Result

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -145,7 +145,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
@@ -167,7 +167,7 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
         
         // Assert
         assert_eq!(
-            200, 
+            400, 
             response.status().as_u16(),
             "The API did not return a 200 OK when the payload was {}.",
             description
