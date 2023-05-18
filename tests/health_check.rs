28d613582e31aa30b6bd592a72b7d22d6ad239af
Vincent Song <dela.vincent.15@gmail.com>
2023-05-18 15:04:32 -0400

    Fix Error Message

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -169,7 +169,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
         assert_eq!(
             400, 
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
+            "The API did not return a 400 Bad Request when the payload was {}.",
             description
         );
     } 
