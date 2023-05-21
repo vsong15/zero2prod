f2191bfc22d95ff5736ae4dfe433b0002ca86360
Vincent Song <dela.vincent.15@gmail.com>
2023-05-21 15:42:53 -0400

    A New Table

--- migrations/20230521193433_make_status_not_null_in_subscriptions.sql
+++ migrations/20230521193433_make_status_not_null_in_subscriptions.sql
@@ -3,10 +3,10 @@
 -- in more details towards the end of this chapter!
 -- `sqlx` does not do it automatically for us.
 BEGIN;
--- Backfill `status` for historical entries
-UPDATE subscriptions
-SET status = 'confirmed'
-WHERE status IS NULL;
--- Make `status` mandatory
-ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+    -- Backfill `status` for historical entries
+    UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE status IS NULL;
+    -- Make `status` mandatory
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
 COMMIT;
