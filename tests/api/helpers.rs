c18e4ceb1c8b5cbf5d64dcff048b7b9f9c331e17
Vincent Song <dela.vincent.15@gmail.com>
2023-05-22 14:25:47 -0400

    Green Test

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -20,6 +20,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 });
 
 pub struct TestApp {
+    pub port: u16,
     pub address: String,
     pub db_pool: PgPool,
     pub email_server: MockServer,
@@ -55,11 +56,12 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application.");
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
     
     TestApp {
-        address,
+        address: format!("http://localhost:{}", application_port),
+        port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
     }
