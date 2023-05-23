5041a565c1d4db21503ee471d1b066b58a04567d
Vincent Song <dela.vincent.15@gmail.com>
2023-05-23 14:21:11 -0400

    Errors At The Edge

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,7 +21,7 @@ impl TryFrom<FormData> for NewSubscriber {
     fn try_from(value: FormData) -> Result<Self, Self::Error> {
         let name = SubscriberName::parse(value.name)?;
         let email = SubscriberEmail::parse(value.email)?;
-        Ok(NewSubscriber { email, name })
+        Ok(Self { email, name })
     }
 }
 
