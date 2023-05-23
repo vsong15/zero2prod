ebd195050e867a558afff64e6f3ce5313558542b
Vincent Song <dela.vincent.15@gmail.com>
2023-05-23 15:39:49 -0400

    The Error Trait

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -172,7 +172,6 @@ fn generate_subscription_token() -> String {
             .collect()
 }    
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl std::fmt::Display for StoreTokenError {
@@ -186,3 +185,28 @@ impl std::fmt::Display for StoreTokenError {
 }
 
 impl ResponseError for StoreTokenError {}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
