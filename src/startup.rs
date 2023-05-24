97cb16a462bd6b47448f538df980c2807a50ed25
Vincent Song <dela.vincent.15@gmail.com>
2023-05-24 15:36:31 -0400

    Green Test

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,4 @@
-use crate::routes::{health_check, subscribe, confirm};
+use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -77,6 +77,7 @@ fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/newsletters", web::post().to(publish_newsletter))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
