6d136337b976e8d2f1714ff16a94506e1a6ac3a8
Vincent Song <dela.vincent.15@gmail.com>
2023-05-26 11:23:26 -0400

    Using A Cryptographic Hash

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -8,6 +8,7 @@ use crate::domain::SubscriberEmail;
 use secrecy::{Secret, ExposeSecret};
 use actix_web::http::header::{HeaderMap, HeaderValue};
 use actix_web::HttpRequest;
+use sha3::Digest;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -168,14 +169,18 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(
+        credentials.password.expose_secret().as_bytes()
+    );
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pool)
     .await
