6c824fc73e556f73cd11ebdcd727c6945f8e3d22
Vincent Song <dela.vincent.15@gmail.com>
2023-05-27 16:18:28 -0400

    Add An HMAC Tag To Protect Query Parameters

--- src/startup.rs
+++ src/startup.rs
@@ -9,6 +9,7 @@ use crate::email_client::EmailClient;
 use crate::configuration::{Settings, DatabaseSettings};
 use sqlx::postgres::PgPoolOptions;
 use crate::routes::{home, login_form, login};
+use secrecy::Secret;
 
 pub struct Application {
     port: u16,
@@ -42,6 +43,7 @@ impl Application {
             connection_pool, 
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -71,6 +73,7 @@ fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -88,8 +91,12 @@ fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
-    })
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
+        })
     .listen(listener)?
     .run();
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
