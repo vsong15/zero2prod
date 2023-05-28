352cb3430d74af2a785e21c95002c9316e54d706
Vincent Song <dela.vincent.15@gmail.com>
2023-05-28 11:56:19 -0400

    Set A Cookie In actix-web

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -6,6 +6,7 @@ use sqlx::PgPool;
 use crate::authentication::AuthError;
 use crate::routes::error_chain_fmt;
 use actix_web::error::InternalError;
+use actix_web::cookie::Cookie;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -43,6 +44,7 @@ pub async fn login(
             };
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
+                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
