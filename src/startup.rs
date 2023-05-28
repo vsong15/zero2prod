b246ddea4c6389188334e134a1aed0df842de66a
Vincent Song <dela.vincent.15@gmail.com>
2023-05-28 14:00:07 -0400

    actix-session

--- src/startup.rs
+++ src/startup.rs
@@ -13,6 +13,8 @@ use secrecy::{Secret, ExposeSecret};
 use actix_web_flash_messages::FlashMessagesFramework;
 use actix_web_flash_messages::storage::CookieMessageStore;
 use actix_web::cookie::Key;
+use actix_session::SessionMiddleware;
+use actix_session::storage::RedisSessionStore;
 
 pub struct Application {
     port: u16,
@@ -20,7 +22,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
     
         let sender_email = configuration
@@ -47,7 +49,8 @@ impl Application {
             email_client,
             configuration.application.base_url,
             configuration.application.hmac_secret,
-        )?;
+            configuration.redis_uri,
+        ).await?;
 
         Ok(Self { port, server })
     }
@@ -71,23 +74,25 @@ pub fn get_connection_pool(
 
 pub struct ApplicationBaseUrl(pub String);
 
-fn run(
+async fn run(
     listener: TcpListener, 
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
-    let message_store = CookieMessageStore::builder(
-        Key::from(hmac_secret.expose_secret().as_bytes())
-    ).build();    
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();   
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())    
+            .wrap(SessionMiddleware::new(redis_store.clone(), secret_key.clone()))
             .wrap(TracingLogger::default())
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/health_check", web::get().to(health_check))
