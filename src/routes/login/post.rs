d6a7d10dcfe62295f91b28578be44fb9d8208d9a
Vincent Song <dela.vincent.15@gmail.com>
2023-05-29 12:13:51 -0400

    Session

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -7,6 +7,7 @@ use crate::authentication::AuthError;
 use crate::routes::error_chain_fmt;
 use actix_web::error::InternalError;
 use actix_web_flash_messages::FlashMessage;
+use actix_session::Session;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,12 +16,13 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    skip(form, pool),
+    skip(form, pool, session),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>, 
     pool: web::Data<PgPool>,
+    session: Session,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -32,6 +34,10 @@ pub async fn login(
         Ok(user_id) => {
             tracing::Span::current()
             .record("user_id", &tracing::field::display(&user_id));
+            session.renew();
+            session
+                .insert("user_id", user_id)
+                .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
                 .finish()
@@ -42,13 +48,17 @@ pub async fn login(
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
-            FlashMessage::error(e.to_string()).send();
-            let response = HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/login"))
-                .finish();
-            Err(InternalError::from_response(e, response))
+            Err(login_redirect(e))
         }
     }
+}
+
+fn login_redirect(e: LoginError) -> InternalError<LoginError> {
+    FlashMessage::error(e.to_string()).send();
+    let response = HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/login"))
+        .finish();
+    InternalError::from_response(e, response)
 }
 
 #[derive(thiserror::Error)]
