840b834d26cd13aac5f04f842d74fdd63ed42586
Vincent Song <dela.vincent.15@gmail.com>
2023-05-29 14:40:00 -0400

    Database Migration

---
+++ migrations/20230529183213_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+'ddf8994f-d522-4659-8d02-c1d479057be6',
+'admin',
+'$argon2id$v=19$m=15000,t=2,p=1$PMBFgXSkE29KEsZMHMxdJg$gAc/eAsLcPLCUGi8HEK1yxHWqgO9g8uX0aq5UXdKWW0'
+);
