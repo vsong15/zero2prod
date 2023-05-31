0f4da50cfb389e56b4030a3716bb449e8ac9a29e
Vincent Song <dela.vincent.15@gmail.com>
2023-05-31 15:05:32 -0400

    Synchronization

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,6 +1,6 @@
 use crate::idempotency::{IdempotencyKey, get_saved_response, save_response};
 use crate::utils::e400;
-
+use crate::idempotency::{try_processing, NextAction};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -25,6 +25,16 @@ pub async fn publish_newsletter(
         idempotency_key 
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    match try_processing(&pool, &idempotency_key, *user_id)
+        .await
+        .map_err(e500)?
+    {
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
+    }
     if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
@@ -47,11 +57,16 @@ pub async fn publish_newsletter(
         }
     }
     FlashMessage::info("The newsletter issue has been published!").send();
+    success_message().send();
     let response = see_other("/admin/newsletters");
     let response = save_response(&pool, &idempotency_key, *user_id, response)
         .await
         .map_err(e500)?;
     Ok(response)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
 }
 
 struct ConfirmedSubscriber {
