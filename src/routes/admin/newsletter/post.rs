457a6cac8e1862332635fdb035c7d630c2ab2c60
Vincent Song <dela.vincent.15@gmail.com>
2023-05-31 16:47:48 -0400

    issue_delivery_queue

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -110,3 +110,25 @@ async fn insert_newsletter_issue(
     .await?;
     Ok(newsletter_issue_id)
 }
+
+#[tracing::instrument(skip_all)]
+async fn enqueue_delivery_tasks(
+    transaction: &mut Transaction<'_, Postgres>,
+    newsletter_issue_id: Uuid,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO issue_delivery_queue (
+            newsletter_issue_id,
+            subscriber_email
+        )
+        SELECT $1, email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+        newsletter_issue_id,
+    )
+    .execute(transaction)
+    .await?;
+    Ok(())
+}
